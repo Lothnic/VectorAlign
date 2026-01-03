c6ded0714435f78bebd6a5cecb428b7ed841f376
Lothnic <joshimayank08012007@gmail.com>
2026-01-03 12:39:13 +0530

    files can now be imported

--- main.py
+++ main.py
@@ -13,4 +13,18 @@ tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 model = AutoModel.from_pretrained(MODEL_NAME).to(device)
 model.eval()
 
+# Loading the data
+with open('data/english.txt','r', encoding='utf-8') as f:
+    english_sentences = f.read().strip().split('\n')
+
+with open('data/hindi.txt','r',encoding='utf-8') as f:
+    hindi_sentences = f.read().strip().split('\n')
+
+length = min(len(english_sentences),len(hindi_sentences))
+print(f'Length Of the data : {length}')
+
+# Preprocessing
+def clean_word(word):
+    return word.strip(string.punctuation).lower()
+
 
